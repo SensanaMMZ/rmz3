3f0ca71d16bcd0a915f2499b6f4a359f13284b13
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-05-28 11:55:04 -0400

    Decompile cyberelf/unk_5 CreateElf5 with CyberElf5 props struct

--- src/cyberelf/unk_5.c
+++ src/cyberelf/unk_5.c
@@ -4,7 +4,29 @@
 
 void Elf5_Die(struct Elf* p);
 
-INCASM("asm/cyberelf/unk_5_p1.inc");
+struct CyberElf5 {
+  OBJECT_HDR;
+  // props (16bytes, offset: 0xB4..)
+  struct Entity* player;  // 0xB4
+  u8 unk_b8[12];          // 0xB8
+};
+static_assert(sizeof(struct CyberElf5) == sizeof(struct Elf));
+
+struct Elf* CreateElf5(struct Entity* e, u8 a1, u8 a2) {
+  struct CyberElf5* p = (struct CyberElf5*)AllocEntityFirst(gElfHeaderPtr);
+  if (p != NULL) {
+    (p->s).taskCol = 16;
+    INIT_ELF_ROUTINE(p, 5);
+    (p->s).tileNum = 0;
+    (p->s).palID = 0;
+    p->player = e;
+    (p->s).work[0] = a1;
+    (p->s).work[1] = a2;
+  }
+  return (struct Elf*)p;
+}
+
+INCASM("asm/cyberelf/unk_5_p1_p2.inc");
 
 void Elf5_Update(struct Elf* p) {
   UpdateMotionGraphic(&p->s);
