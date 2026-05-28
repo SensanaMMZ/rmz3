5a4e92fc04f52c099be14d5270677698287d6ec3
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-05-28 17:56:46 -0400

    Decompile NurseE_Die (byte-encoded)

--- src/cyberelf/nurse_e.c
+++ src/cyberelf/nurse_e.c
@@ -1,10 +1,19 @@
 #include "cyberelf.h"
 #include "entity.h"
 #include "global.h"
+#include "vfx.h"
 
 // コロップ、ソロップ(つまり、画面上を飛行するエルフ？)
 
-INCASM("asm/cyberelf/nurse_e.inc");
+INCASM("asm/cyberelf/nurse_e_p1.inc");
+
+void NurseE_Die(struct Elf* p) {
+  FUN_080bfce8(&(p->s).coord, 0);
+  (p->s).flags &= ~DISPLAY;
+  SET_ELF_ROUTINE(p, ENTITY_EXIT);
+}
+
+INCASM("asm/cyberelf/nurse_e_p2.inc");
 
 void NurseE_Init(struct Elf* p);
 void NurseE_Update(struct Elf* p);
