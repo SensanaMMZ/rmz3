69044af33658224b0b0e212bf6071d0e0c14ff2d
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-05-31 03:40:17 -0400

    Decompile unk_64 spawners + Ghost64_Init (7 funcs)
    Ported from upstream: 6 VFX_UNK_064 spawn helpers (oz rekkoha/messenkou
    family, double-write xflip idiom) + Ghost64_Init. Whole unk_64_p1_p2.inc
    consumed, removed.

--- src/vfx/unk_64.c
+++ src/vfx/unk_64.c
@@ -16,7 +16,117 @@ void oz_080c39a0(struct Entity* e) {
   }
 }
 
-INCASM("asm/vfx/unk_64_p1_p2.inc");
+static const u8 sInitModes[7];
+void Ghost64_Update(struct VFX* p);
+
+void FUN_080c39e8(struct Entity* e) {
+  struct VFX* p = (struct VFX*)AllocEntityFirst(gVFXHeaderPtr);
+  if (p != NULL) {
+    (p->s).taskCol = 1;
+    INIT_VFX_ROUTINE(p, VFX_UNK_064);
+    (p->s).tileNum = 0;
+    (p->s).palID = 0;
+    (p->s).unk_28 = e;
+    (p->s).work[0] = 1;
+    {
+      s32 ex = (e->coord).x;
+      s32 x = (p->s).coord.x = ex + PIXEL(12);
+      if ((e->flags & X_FLIP) != 0) {
+        x = ex - PIXEL(12);
+      }
+      (p->s).coord.x = x;
+    }
+    (p->s).coord.y = (e->coord).y - PIXEL(32);
+  }
+}
+
+void oz_Rekkoha_080c3a5c(struct Entity* e) {
+  struct VFX* p = (struct VFX*)AllocEntityFirst(gVFXHeaderPtr);
+  if (p != NULL) {
+    (p->s).taskCol = 1;
+    INIT_VFX_ROUTINE(p, VFX_UNK_064);
+    (p->s).tileNum = 0;
+    (p->s).palID = 0;
+    (p->s).unk_28 = e;
+    (p->s).work[0] = 3;
+    {
+      s32 ex = (e->coord).x;
+      s32 x = (p->s).coord.x = ex + PIXEL(12);
+      if ((e->flags & X_FLIP) != 0) {
+        x = ex - PIXEL(12);
+      }
+      (p->s).coord.x = x;
+    }
+    (p->s).coord.y = (e->coord).y - PIXEL(32);
+  }
+}
+
+void ozMessenkou2(struct Entity* e) {
+  struct VFX* p = (struct VFX*)AllocEntityFirst(gVFXHeaderPtr);
+  if (p != NULL) {
+    (p->s).taskCol = 1;
+    INIT_VFX_ROUTINE(p, VFX_UNK_064);
+    (p->s).tileNum = 0;
+    (p->s).palID = 0;
+    (p->s).work[0] = 2;
+    {
+      s32 ex = (e->coord).x;
+      s32 x = (p->s).coord.x = ex - PIXEL(5);
+      if ((e->flags & X_FLIP) != 0) {
+        x = ex + PIXEL(5);
+      }
+      (p->s).coord.x = x;
+    }
+    (p->s).coord.y = (e->coord).y + PIXEL(1);
+  }
+}
+
+void oz_080c3b44(struct Entity* e) {
+  struct VFX* p = (struct VFX*)AllocEntityFirst(gVFXHeaderPtr);
+  if (p != NULL) {
+    (p->s).taskCol = 1;
+    INIT_VFX_ROUTINE(p, VFX_UNK_064);
+    (p->s).tileNum = 0;
+    (p->s).palID = 0;
+    (p->s).work[0] = 4;
+    (p->s).coord.x = (e->coord).x;
+    (p->s).coord.y = (e->coord).y - PIXEL(22);
+  }
+}
+
+void oz_080c3b9c(struct Entity* e) {
+  struct VFX* p = (struct VFX*)AllocEntityFirst(gVFXHeaderPtr);
+  if (p != NULL) {
+    (p->s).taskCol = 1;
+    INIT_VFX_ROUTINE(p, VFX_UNK_064);
+    (p->s).tileNum = 0;
+    (p->s).palID = 0;
+    (p->s).work[0] = 5;
+    (p->s).coord.x = (e->coord).x;
+    (p->s).coord.y = (e->coord).y - PIXEL(22);
+  }
+}
+
+void FUN_080c3bf4(s32 x, s32 y) {
+  struct VFX* p = (struct VFX*)AllocEntityFirst(gVFXHeaderPtr);
+  if (p != NULL) {
+    (p->s).taskCol = 1;
+    INIT_VFX_ROUTINE(p, VFX_UNK_064);
+    (p->s).tileNum = 0;
+    (p->s).palID = 0;
+    (p->s).work[0] = 6;
+    (p->s).coord.x = x, (p->s).coord.y = y;
+  }
+}
+
+void Ghost64_Init(struct VFX* p) {
+  SET_VFX_ROUTINE(p, ENTITY_UPDATE);
+  (p->s).mode[1] = sInitModes[(p->s).work[0]];
+  (p->s).flags |= FLIPABLE;
+  (p->s).flags |= DISPLAY;
+  InitNonAffineMotion(&p->s);
+  Ghost64_Update(p);
+}
 
 void Ghost64_Update(struct VFX* p) {
   (sUpdates[(p->s).mode[1]])(p);
