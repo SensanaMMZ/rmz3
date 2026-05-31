4898f8139e15ba69012ca4a2520eeb4979675d2e
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-05-31 02:30:58 -0400

    Decompile Elf0_Init + Elf0_Update (HP-gauge cyberelf)
    Ported from upstream mmzret/rmz3; adapted to local struct CyberElf0
    layout and the file-scope gElf0Updates dispatch array.

--- src/cyberelf/unk_0.c
+++ src/cyberelf/unk_0.c
@@ -1,25 +1,30 @@
 #include "cyberelf.h"
 #include "entity.h"
 #include "global.h"
+#include "stagerun.h"
+#include "trig.h"
 #include "zero.h"
 
 struct CyberElf0 {
   OBJECT_HDR;
   // props (16bytes, offset: 0xB4..)
-  u8 unk_b4[8];         // 0xB4
-  struct Zero* player;  // 0xBC
-  u8 unk_c0[4];         // 0xC0
+  struct Coord coord_b4;  // 0xB4
+  struct Zero* player;    // 0xBC
+  u8 unk_c0;              // 0xC0
+  u8 unk_c1;              // 0xC1
 };
 static_assert(sizeof(struct CyberElf0) == sizeof(struct Elf));
 
-void Elf0_Init(struct Elf* p);
-void Elf0_Update(struct Elf* p);
+void Elf0_Init(struct CyberElf0* p);
+void Elf0_Update(struct CyberElf0* p);
 void Elf0_Die(struct Elf* p);
+
+extern const ElfFunc gElf0Updates[3];
 
 // clang-format off
 const ElfRoutine gElf0Routine = {
-    [ENTITY_INIT] =      Elf0_Init,
-    [ENTITY_UPDATE] =    Elf0_Update,
+    [ENTITY_INIT] =      (ElfFunc)Elf0_Init,
+    [ENTITY_UPDATE] =    (ElfFunc)Elf0_Update,
     [ENTITY_DIE] =       Elf0_Die,
     [ENTITY_DISAPPEAR] = DeleteElf,
     [ENTITY_EXIT] =      (ElfFunc)DeleteEntity,
@@ -40,7 +45,49 @@ struct Entity* CreateElf0(struct Zero* z, u8 breed, u8 availability, u8 _) {
   return (struct Entity*)p;
 }
 
-INCASM("asm/cyberelf/unk_0_pre.inc");
+void Elf0_Init(struct CyberElf0* p) {
+  struct Zero* z = p->player;
+  struct Rect r = gZeroRanges[z->posture];
+  gPause = TRUE;
+  InitNonAffineMotion(&p->s);
+  ResetDynamicMotion(&p->s);
+  (p->s).flags |= DISPLAY;
+  (p->s).flags |= FLIPABLE;
+  SetMotion(&p->s, GetElfMotion(0));
+  UpdateMotionGraphic(&p->s);
+  (p->s).spr.xflip = FALSE;
+  (p->s).spr.oam.xflip = FALSE;
+  (p->s).flags &= ~X_FLIP;
+  (p->s).spr.oam.priority = 0;
+  (p->s).coord.x = (z->s).coord.x + r.x;
+  (p->s).coord.y = (z->s).coord.y + r.y;
+  (p->coord_b4).x = PIXEL(10);
+  (p->coord_b4).y = PIXEL(80);
+  if ((p->s).work[0] == 0) {
+    (p->coord_b4).y -= PIXEL(getMaxHP1x(z) * 2);
+  } else {
+    (p->coord_b4).y -= PIXEL(getMaxHP1x(z));
+  }
+  (p->s).unk_coord.x = PIXEL(120) - (&gStageRun.vm.camera.viewport)->x + (p->s).coord.x;
+  (p->s).unk_coord.y = PIXEL(80) - (&gStageRun.vm.camera.viewport)->y + (p->s).coord.y;
+  p->unk_c1 = 32;
+  SET_ELF_ROUTINE(p, ENTITY_UPDATE);
+  Elf0_Update(p);
+}
+
+void Elf0_Update(struct CyberElf0* p) {
+  u8 idx;
+  UpdateMotionGraphic(&p->s);
+  (gElf0Updates[(p->s).mode[1]])((struct Elf*)p);
+  if ((p->s).mode[0] == ENTITY_UPDATE) {
+    (p->s).coord.x = (p->s).unk_coord.x - PIXEL(120);
+    (p->s).coord.x += (&gStageRun.vm.camera.viewport)->x;
+    (p->s).coord.y = (p->s).unk_coord.y - PIXEL(80);
+    (p->s).coord.y += (&gStageRun.vm.camera.viewport)->y;
+    idx = p->unk_c0;
+    (p->s).coord.y += SIN(idx << 3) << 2;
+  }
+}
 
 void Elf0_Die(struct Elf* p) {
   gPause = 0;
