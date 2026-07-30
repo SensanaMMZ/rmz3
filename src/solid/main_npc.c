543c95363ee8c2d594641b9da59269c83cebb3fc
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-07-30 13:13:30 -0400

    decompile UpdateBlockingColbor

--- src/solid/main_npc.c
+++ src/solid/main_npc.c
@@ -5,6 +5,9 @@
 #include "solid.h"
 #include "story.h"
 #include "text.h"
+#include "text_window.h"
+#include "zero.h"
+#include "physics.h"
 
 static void MainNPC_Init(struct Solid* p);
 static void MainNPC_Update(struct Solid* p);
@@ -111,6 +114,137 @@ static void deleteMainNPC(struct Solid* p) {
 NAKED static void UpdateCiel(struct Solid* p) { INCCODE("asm/wip/UpdateCiel.inc"); }
 
 INCASM("asm/solid/main_npc.inc");
+
+extern const struct Collision Collision_ARRAY_083713e0[2];
+
+void UpdateBlockingColbor(struct Solid* p) {
+  switch ((p->s).mode[1]) {
+    case 0: {
+      u32 g6 = (u8)(gCurStory.s.gameflags[1] & 2);
+      if (g6 != 0) {
+        {
+          u32 fl = (p->s).flags & 0xFE;
+          fl &= 0xFD;
+          (p->s).flags = fl;
+        }
+        {
+          u8* w = (u8*)p + 0x8c;
+          s32 z = 0;
+          *(u32*)w = z;
+          asm("" : "+r"(w));
+          w += 4;
+          *(u32*)w = z;
+          asm("" : "+r"(w));
+          w += 4;
+          asm("" : "+r"(w));
+          *w = z;
+        }
+        (p->s).flags &= 0xFB;
+        {
+          u32 tbl = (u32)gSolidFnTable;
+          u32 id = ((p->s).id) << 2;
+          EntityFunc** rt = (EntityFunc**)(tbl + id);
+          *(u32*)((p->s).mode) = 3;
+          (p->s).onUpdate = (void*)((*rt)[3]);
+        }
+        break;
+      }
+      {
+        u32 fl = (p->s).flags;
+        register u32 t asm("r0");
+        u32 two;
+        t = 1;
+        asm volatile("" : "+r"(t));
+        t |= fl;
+        two = 2;
+        asm("" : "+r"(two));
+        t |= two;
+        (p->s).flags = t;
+      }
+      InitNonAffineMotion(&p->s);
+      (p->s).flags &= 0xEF;
+      ((p->s).spr).xflip = g6;
+      {
+        u8* oa = (u8*)p + 0x4a;
+        s32 ov = *oa;
+        s32 m11 = -0x11;
+        m11 &= ov;
+        *oa = m11;
+      }
+      SetMotion(&p->s, 0x8200);
+      (p->s).flags |= 4;
+      {
+        struct Body* body = &p->body;
+        InitBody(body, Collision_ARRAY_083713e0, &(p->s).coord, 0x40);
+        body->parent = (struct CollidableEntity*)p;
+        body->fn = (void*)g6;
+      }
+      (p->s).coord.y = FUN_08009f6c((p->s).coord.x, (p->s).coord.y);
+      (p->s).mode[1]++;
+    }
+      // fallthrough
+    case 1: {
+      UpdateMotionGraphic(&p->s);
+      if (!((p->body).status & 0x20000000)) {
+        break;
+      }
+      if (gInChat == 0) {
+        break;
+      }
+      {
+        u8* cm = (u8*)&gCollisionManager;
+        asm("" : "+r"(cm));
+        if (*(void**)(cm + 0x628) != (void*)&p->body) {
+          break;
+        }
+      }
+      {
+        u32 xf = 0;
+        if ((pZero2->s).coord.x - (p->s).coord.x > 0) {
+          xf = 1;
+        }
+        if (xf != 0) {
+          (p->s).flags |= 0x10;
+        } else {
+          (p->s).flags &= 0xEF;
+        }
+        {
+          register u32 xfc asm("r1");
+          xfc = xf;
+          asm volatile("" : "+r"(xfc));
+          ((p->s).spr).xflip = xfc;
+          {
+            u8* oa = (u8*)p + 0x4a;
+            u32 sh4 = xfc << 4;
+            s32 ov, m11;
+            ov = *oa;
+            m11 = -0x11;
+            m11 &= ov;
+            m11 |= sh4;
+            *oa = m11;
+          }
+        }
+      }
+      PrintNormalMessage(0x1306);
+      (p->s).mode[1]++;
+      break;
+    }
+    case 2: {
+      u32 tw;
+      UpdateMotionGraphic(&p->s);
+      {
+        struct TextWindowText* tx = &gTextWindow.text;
+        tw = tx->mode;
+      }
+      if (tw != 0) {
+        break;
+      }
+      gInChat = tw;
+      (p->s).mode[1] = 1;
+      break;
+    }
+  }
+}
 
 const struct Collision Collision_ARRAY_08371380[2] = {
     {
