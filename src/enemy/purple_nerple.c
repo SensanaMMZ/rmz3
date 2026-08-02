43466c5b908e651bb1e6cc50f2fe792e79bbaf63
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-02 06:18:22 -0400

    decompile FUN_08076b30

--- src/enemy/purple_nerple.c
+++ src/enemy/purple_nerple.c
@@ -512,6 +512,61 @@ void FUN_08076594(struct Enemy* p) {
 
 INCASM("asm/enemy/purple_nerple_p2_p2_p4_b.inc");
 
+#include "mission.h"
+
+void TryDropZakoDisk(struct Enemy* p, struct Coord* c);
+struct Entity* FUN_080b7f70(struct Entity* e, struct Coord* c, motion_t* motions, u8 len);
+static const motion_t sMotions[9];
+
+// 0x08076b30
+void FUN_08076b30(struct Enemy* p) {
+  struct Coord c;
+  struct Coord* co;
+  {
+    register u8 f asm("r0");
+    register u8 t asm("r1");
+    register u8 k2 asm("r1");
+    u8* qq = (u8*)p + 0x8c;
+    s32 z;
+    asm("" : "+r"(qq));
+    z = 0;
+    *(s32*)qq = z;
+    asm("" : "+r"(qq));
+    qq += 4;
+    asm("" : "+r"(qq));
+    *(s32*)qq = z;
+    asm("" : "+r"(qq));
+    qq += 4;
+    asm("" : "+r"(qq));
+    *qq = z;
+    t = (p->s).flags;
+    f = 0xFB;
+    f &= t;
+    asm volatile("" ::"r"(t));
+    k2 = 0xFE;
+    f &= k2;
+    (p->s).flags = f;
+  }
+  c.x = (p->s).coord.x;
+  c.y = (p->s).coord.y - 0x1000;
+  CreateSmoke(1, &c);
+  PlaySound(0x2a);
+  if (*((u8*)p + 0xb9) != 0) {
+    FUN_080b7ffc(&p->s, &c, (motion_t*)sMotions, 3);
+  } else {
+    FUN_080b7f70(&p->s, &c, (motion_t*)sMotions, 3);
+  }
+  co = &(p->s).coord;
+  TryDropItem(3, co);
+  if (gMission.enemyCount <= 0x270E) {
+    gMission.enemyCount++;
+  }
+  TryDropZakoDisk(p, co);
+  SET_ENEMY_ROUTINE(p, 4);
+}
+
+INCASM("asm/enemy/purple_nerple_p2_p2_p4_b2.inc");
+
 extern const motion_t sMotions[9];
 struct Entity* FUN_080b7f70(struct Entity* e, struct Coord* c, motion_t* motions, u8 len);
 
