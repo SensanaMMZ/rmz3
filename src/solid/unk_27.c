7a7141d361a9525e9fe09292b250b9f321a58d13
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-02 05:50:41 -0400

    decompile Solid27_Init

--- src/solid/unk_27.c
+++ src/solid/unk_27.c
@@ -47,6 +47,53 @@ void FUN_080d8fe4(struct Entity* p) {
   }
 }
 
+extern const struct Collision Collision_ARRAY_083710f4[5];
+void Solid27_Update(struct Solid* p);
+
+// 0x080d8ff4
+void Solid27_Init(struct Solid* p) {
+  register struct Solid* pp asm("r5");
+  register s32 z2 asm("r2");
+  register u8 fv asm("r1");
+  s32 z6;
+  struct Body* body;
+  register const struct Collision* col asm("r1");
+  pp = p;
+  z2 = 0;
+  z6 = 0;
+  (pp->s).work[3] = z6;
+  (pp->s).work[2] = z6;
+  {
+    u8 t = (pp->s).flags;
+    fv = FLIPABLE;
+    fv |= t;
+    asm("" : "+r"(z2));
+    fv |= z2;
+    (pp->s).flags = fv;
+  }
+  if ((pp->s).work[0] == 0) {
+    fv |= COLLIDABLE;
+    (pp->s).flags = fv;
+    body = &pp->body;
+    col = &Collision_ARRAY_083710f4[0];
+  } else {
+    fv |= COLLIDABLE;
+    (pp->s).flags = fv;
+    body = &pp->body;
+    {
+      register u8 w1 asm("r0");
+      w1 = (pp->s).work[1];
+      col = &Collision_ARRAY_083710f4[1 + w1];
+    }
+  }
+  InitBody(body, col, &(pp->s).coord, 0);
+  body->parent = (struct CollidableEntity*)pp;
+  body->fn = (BodyFunc)z6;
+  SET_SOLID_ROUTINE(pp, ENTITY_UPDATE);
+  Solid27_Update(pp);
+}
+
+
 INCASM("asm/solid/unk_27_p1_p3.inc");
 
 void Solid27_Die(struct Solid* p) {}
