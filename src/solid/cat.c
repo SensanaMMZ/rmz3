168eeb2b8809ba27cd0cd09dc3bb994211d0258c
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-02 14:22:31 -0400

    decompile FUN_080dd5e0

--- src/solid/cat.c
+++ src/solid/cat.c
@@ -67,6 +67,38 @@ static void Cat_Die(struct Solid* p) { SET_SOLID_ROUTINE(p, ENTITY_EXIT); }
 
 INCASM("asm/solid/cat_a.inc");
 
+// 0x080DD5E0
+void FUN_080dd5e0(struct Solid* p) {
+  register s32 one asm("r4");
+  register s32 z asm("r6");
+  u32 r;
+  InitNonAffineMotion(&p->s);
+  {
+    register u8 fv asm("r0");
+    register u8 k asm("r1");
+    fv = (p->s).flags;
+    one = 1;
+    z = 0;
+    asm("" : "+r"(z));
+    fv |= one;
+    k = FLIPABLE;
+    fv |= k;
+    (p->s).flags = fv;
+  }
+  SetMotion(&p->s, MOTION(0xD9, 0x00));
+  r = RANDOM(RNG_0202f388) & one;
+  SET_XFLIP(p, r);
+  (p->s).taskCol = 0x1D;
+  (p->s).coord.y = FUN_08009f6c((p->s).coord.x, (p->s).coord.y) + 0x100;
+  *(s32*)((u8*)p + 0xb4) = (p->s).coord.x;
+  *(s32*)((u8*)p + 0xb8) = (p->s).coord.y;
+  asm volatile("" ::"r"(z));
+  SET_SOLID_ROUTINE(p, ENTITY_UPDATE);
+  Cat_Update(p);
+}
+
+INCASM("asm/solid/cat_a_post.inc");
+
 #include "motion.h"
 
 void FUN_080dd894(struct Solid* p) {
