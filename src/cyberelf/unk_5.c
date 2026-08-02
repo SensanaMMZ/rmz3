536a151ea4ba05a5a8fa86f969749d8f119c73de
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-01 21:49:43 -0400

    decompile Elf5_Die

--- src/cyberelf/unk_5.c
+++ src/cyberelf/unk_5.c
@@ -34,7 +34,38 @@ void Elf5_Update(struct Elf* p) {
   Elf5_Die(p);
 }
 
-INCASM("asm/cyberelf/unk_5_p2.inc");
+#include "mission.h"
+
+struct Entity* FUN_080bfce8(struct Coord* c, s32 n);
+
+// 0x080E35A8
+void Elf5_Die(struct Elf* p0) {
+  register struct Elf* p asm("r4");
+  struct Entity* q;
+  register u16* h asm("r3");
+  register u8 (**tbl)[CYBERELF_LENGTH] asm("r1");
+  register s32 k asm("r5");
+  p = p0;
+  q = *(struct Entity**)((u8*)p + 0xb4);
+  gPause = FALSE;
+  tbl = &gUnlockedElfPtr;
+  h = (u16*)((u8*)q + 0xb4);
+  k = 0x121;
+  {
+    u8* e = (**tbl) + *((u8*)q + k);
+    *e |= 2;
+  }
+  h[2] += 3;
+  {
+    struct PlayInfo* pi = gMission.unk_00;
+    if (pi->fusionCount <= 0x62) {
+      pi->fusionCount++;
+    }
+  }
+  FUN_080bfce8(&(p->s).coord, 0);
+  (p->s).flags &= ~DISPLAY;
+  SET_ELF_ROUTINE(p, ENTITY_EXIT);
+}
 
 void Elf5_Init(struct Elf* p);
 void Elf5_Update(struct Elf* p);
