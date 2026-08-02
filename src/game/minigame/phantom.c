0b35bdb00f487cb0f7abca86d2078536e2e655dd
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-02 00:34:33 -0400

    decompile phantomMinigame_080fc390

--- src/game/minigame/phantom.c
+++ src/game/minigame/phantom.c
@@ -17,14 +17,57 @@ void MinigamePhantom_DrawScoreHiscore(struct GameState* g) {
 
 INCASM("asm/minigame/phantom.inc");
 
+#include "vfx.h"
+#include "sound.h"
+
+// 0x080FC390
+bool32 phantomMinigame_080fc390(struct GameState* g);
+bool32 phantomMinigame_080fc390(struct GameState* g) {
+  struct MinigameState* s = (struct MinigameState*)(g->sceneState).raw;
+  u16 z = 0;
+  *(u16*)&s->unk_10[0] = z;
+  *(u16*)&s->unk_10[2] = z;
+  switch (s->unk_00[1]) {
+    case 0:
+      fadeoutBGM(0xA8);
+      *(s32*)&s->unk_40[0] = 0x3C;
+      s->unk_00[1]++;
+    case 1:
+      *(s32*)&s->unk_40[0] -= 1;
+      if (*(s32*)&s->unk_40[0] != 0) {
+        goto end1;
+      }
+      goto inc;
+    case 2:
+      *(struct VFX**)&s->unk_0c = CreateMissionAlert(4);
+      s->unk_00[1]++;
+    case 3:
+      if (((*(struct VFX**)&s->unk_0c)->s).mode[0] <= 1) {
+        return 1;
+      }
+    inc:
+      s->unk_00[1]++;
+      break;
+    case 4:
+      gGameState.frames = 0x40;
+      s->unk_00[1]++;
+    case 5:
+      if (gGameState.frames != 0x20) {
+        break;
+      }
+      return 0;
+  }
+end1:
+  return 1;
+}
+
 void phantomMinigame_080fc13c(struct GameState* p);
 void phantomMinigame_080fc1b8(struct GameState* p);
-void phantomMinigame_080fc390(struct GameState* p);
 
 const GameLoopFunc PhantomMinigameLoops[3] = {
     phantomMinigame_080fc13c,
     phantomMinigame_080fc1b8,
-    phantomMinigame_080fc390,
+    (GameLoopFunc)phantomMinigame_080fc390,
 };
 
 bool32 exitPhantomMinigame(struct GameState* g) {
