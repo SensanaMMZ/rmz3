b90065aa041825c7676eb35258ee26e5ab466de7
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-02 22:29:43 -0400

    decompile FUN_080c7700

--- src/vfx/unk_75.c
+++ src/vfx/unk_75.c
@@ -98,6 +98,87 @@ static void Ghost75_Die(struct Entity* p) {
 
 static void nop_080c76fc(void* _) {}
 
+// 0x080C7700
+void FUN_080c7700(struct VFX* p) {
+  s32 t = (p->s).work[2] - 1;
+  (p->s).work[2] = t;
+  t <<= 24;
+  if (t == 0) {
+    register s32 r asm("r4");
+    struct Coord c;
+    CreateSmoke(2, &(p->s).coord);
+    {
+      register u32 rv asm("r1");
+      register u32 ka asm("r0");
+      rv = RNG_0202f388;
+      ka = 0x343FD;
+      asm volatile("add %0, %1, #0" : "=&l"(r) : "l"(rv));
+      r *= ka;
+      ka = 0x269EC3;
+      r += ka;
+      r <<= 1;
+      RNG_0202f388 = (u32)r >> 1;
+      r = (u32)r >> 0x11;
+      ka = 3;
+      r &= ka;
+    }
+    c.x = (p->s).coord.x;
+    c.y = (p->s).coord.y;
+    {
+      register s32 m5 asm("r5");
+      {
+        register u8 w0 asm("r1");
+        register s32 m2 asm("r2");
+        register struct Coord* cp asm("r0");
+        w0 = (p->s).work[0];
+        m5 = 0xD50E;
+        cp = &c;
+        asm volatile("add %0, %1, #0" : "=&l"(m2) : "l"(m5));
+        ((void (*)(struct Coord*, s32, s32, s32))FUN_080c7570)(cp, w0, m2, r);
+      }
+      {
+        register u8 w1 asm("r1");
+        register s32 m3 asm("r2");
+        register struct Coord* cp1 asm("r0");
+        w1 = (p->s).work[0];
+        m3 = 0xD50D;
+        cp1 = &c;
+        ((void (*)(struct Coord*, s32, s32, s32))FUN_080c7570)(cp1, w1, m3, r);
+      }
+      {
+        register u8 w2 asm("r1");
+        register s32 m6 asm("r2");
+        register struct Coord* cp2 asm("r0");
+        w2 = (p->s).work[0];
+        cp2 = &c;
+        asm volatile("add %0, %1, #0" : "=&l"(m6) : "l"(m5));
+        ((void (*)(struct Coord*, s32, s32, s32))FUN_080c7570)(cp2, w2, m6, r);
+      }
+    }
+    PlaySound(0x2A);
+    SET_VFX_ROUTINE(p, ENTITY_DIE);
+    return;
+  }
+
+  switch ((p->s).mode[2]) {
+    case 0:
+      (p->s).work[2] = 0x1E;
+      (p->s).d.y = -0x180;
+      SetMotion(&p->s, MOTION(0xD5, 0x06));
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    case 1:
+      (p->s).d.y += 0x10;
+      if ((p->s).d.y > 0x700) {
+        (p->s).d.y = 0x700;
+      }
+      (p->s).coord.y += (p->s).d.y;
+      (p->s).coord.x += (p->s).d.x;
+      UpdateMotionGraphic(&p->s);
+      break;
+  }
+}
+
 INCASM("asm/vfx/unk_75.inc");
 
 // --------------------------------------------
