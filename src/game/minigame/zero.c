911ef025bf909f6e440ea966e8ed55afc6aad4cd
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-03 13:20:31 -0400

    decompile zeroMinigamePhase2

--- src/game/minigame/zero.c
+++ src/game/minigame/zero.c
@@ -71,14 +71,80 @@ bool32 zeroMinigamePhase0(struct GameState* g) {
 
 INCASM("asm/minigame/zero_b.inc");
 
+// 0x080F9750
+bool32 zeroMinigamePhase2(struct GameState* g) {
+  struct MinigameState* s = &(g->sceneState).mg;
+  u8 b1[8];
+  u8 b2[12];
+  switch (s->unk_06) {
+    case 0:
+      *(u16*)s->unk_00 = 0x3c;
+      s->unk_06++;
+      /* fallthrough */
+    case 1: {
+      s32 raw = *(u16*)s->unk_00 - 1;
+      *(u16*)s->unk_00 = raw;
+      if ((u16)raw != 0) {
+        break;
+      }
+      goto inc;
+    }
+    case 2:
+      *(struct VFX**)s->unk_10 = CreateMissionAlert(4);
+      fadeoutBGM(0xad);
+      s->unk_06++;
+      /* fallthrough */
+    case 3:
+      if (((*(struct VFX**)s->unk_10)->s).mode[0] <= 1) {
+        break;
+      }
+    inc:
+      s->unk_06++;
+      break;
+    case 4:
+      gGameState.frames = 0x40;
+      s->unk_06++;
+      /* fallthrough */
+    case 5:
+      if (gGameState.frames != 0x20) {
+        break;
+      }
+      {
+        s32 z = 0;
+        *(u16*)s->unk_00 = z;
+        s->unk_06 = z;
+        return z;
+      }
+  }
+  memcpy(b1, Unicode_SCORE_0810e248, 6);
+  PrintUnicodeString(b1, 1, 0);
+  {
+    u32 score = *(u16*)s->unk_38;
+    PrintMinigameNumber(score, 0xa, 0);
+    if (score > (u32)s->unk_3c) {
+      s->unk_3c = score;
+    }
+  }
+  memcpy(b2, Unicode_HI_SCORE_0810e250, 9);
+  PrintUnicodeString(b2, 0xf, 0);
+  {
+    register u32 hs0 asm("r4");
+    u32 hs;
+    hs0 = s->unk_3c;
+    asm volatile("add %0, %1, #0" : "=&l"(hs) : "l"(hs0));
+    PrintMinigameNumber(hs, 0x1b, 0);
+  }
+  return 1;
+}
+
 bool32 zeroMinigamePhase0(struct GameState* g);
 void zeroMinigamePhase1(struct GameState* g);
-void zeroMinigamePhase2(struct GameState* g);
+bool32 zeroMinigamePhase2(struct GameState* g);
 
 const GameLoopFunc ZeroMinigameLoops[3] = {
     (GameLoopFunc)zeroMinigamePhase0,
     zeroMinigamePhase1,
-    zeroMinigamePhase2,
+    (GameLoopFunc)zeroMinigamePhase2,
 };
 
 // clang-format off
