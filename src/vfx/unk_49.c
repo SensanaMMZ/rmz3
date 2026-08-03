c4a9105549dc66b6d92f2c3fb87c81ac72a90b38
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-02 20:10:46 -0400

    decompile FUN_080c0400

--- src/vfx/unk_49.c
+++ src/vfx/unk_49.c
@@ -78,7 +78,107 @@ void VFX49_Die(struct VFX* vfx) {
   SET_VFX_ROUTINE(vfx, ENTITY_EXIT);
 }
 
-INCASM("asm/vfx/unk_49_post.inc");
+// 0x080C0400
+void FUN_080c0400(struct VFX* p) {
+  register s32 m asm("r5");
+  m = (p->s).mode[2];
+  switch (m) {
+    case 0:
+      (p->s).taskCol = 0x19;
+      SetMotion(&p->s, (RANDOM(RNG_0202f388) & 3) + 0x5102);
+      (p->s).d.y = m;
+      (p->s).work[2] = 0x46;
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    case 1: {
+      register s32 t asm("r1");
+      register s32 z asm("r2");
+      register u32 u asm("r0");
+      register u8 f asm("r0");
+      {
+        register s32 dy asm("r1");
+        register s32 cy asm("r0");
+        dy = (p->s).d.y;
+        dy += 4;
+        (p->s).d.y = dy;
+        cy = (p->s).coord.y;
+        cy += dy;
+        (p->s).coord.y = cy;
+      }
+      {
+        register s32 tl asm("r0");
+        tl = (p->s).work[2];
+        t = tl - 1;
+      }
+      z = 0;
+      asm volatile("" : "+r"(z));
+      (p->s).work[2] = t;
+      u = (u8)t;
+      if (u <= 9) {
+        s32 k = 1;
+        t &= k;
+        asm volatile("" ::: "cc");
+        if (t == 0) {
+          goto clr;
+        }
+        {
+          register u8 fv asm("r0");
+          register s32 one asm("r1");
+          fv = (p->s).flags;
+          one = 1;
+          fv |= one;
+          asm volatile("" : "+r"(fv));
+          f = fv;
+        }
+        goto fs;
+      }
+      if (u > 0x13) {
+        goto after;
+      }
+      {
+        s32 k2 = 2;
+        t &= k2;
+        asm volatile("" : "+r"(t));
+        if (t == 0) {
+          goto clr;
+        }
+        {
+          register u8 fv2 asm("r0");
+          register s32 one2 asm("r1");
+          fv2 = (p->s).flags;
+          one2 = 1;
+          fv2 |= one2;
+          f = fv2;
+        }
+        goto fs;
+      }
+    clr : {
+      register u8 h asm("r1");
+      h = (p->s).flags;
+      asm("" : "+r"(h));
+      f = 0xFE;
+      f &= h;
+    }
+    fs:
+      (p->s).flags = f;
+    after:
+      UpdateMotionGraphic(&p->s);
+      if ((p->s).work[2] == 0) {
+        register u8 g2 asm("r0");
+        register u8 h2 asm("r1");
+        h2 = (p->s).flags;
+        asm("" : "+r"(h2));
+        g2 = 0xFE;
+        g2 &= h2;
+        h2 = 0xFD;
+        g2 &= h2;
+        (p->s).flags = g2;
+        SET_VFX_ROUTINE(p, ENTITY_DISAPPEAR);
+      }
+      break;
+    }
+  }
+}
 
 u8 GetEntityPalID(struct Entity* p);
 
