2ea46e4e5c7fa706c32c5f0e3122872bb37a9230
SensanaMMZ <305674455+SensanaMMZ@users.noreply.github.com>
2026-08-05 01:17:37 -0400

    decompile FUN_080a202c

--- src/projectile/hellbat.c
+++ src/projectile/hellbat.c
@@ -1,5 +1,6 @@
 #include "collision.h"
 #include "global.h"
+#include "stagerun.h"
 #include "projectile.h"
 
 static const ProjectileFunc* const sUpdates[5];
@@ -518,7 +519,283 @@ void FUN_080a2020(struct Projectile* p) {
   (p->s).mode[2] = 0;
 }
 
-INCASM("asm/projectile/hellbat_p3_p1.inc");
+s32 PushoutToUp1(s32 x, s32 y);
+s32 PushoutToLeft1(s32 x, s32 y);
+s32 PushoutToRight1(s32 x, s32 y);
+struct VFX* createHellbatElectricBeam(struct Entity* e, struct Coord* c, u8 a2, u8 a3);
+
+// 0x080A202C
+void FUN_080a202c(struct Projectile* p) {
+  struct Coord c1;
+  struct Coord c2;
+  if (((p->s).unk_28)->mode[0] > 1) {
+    register u8* a asm("r0");
+    register s32 z asm("r1");
+    a = (u8*)p + 0x8c;
+    z = 0;
+    asm volatile("str %0, [%1]" ::"l"(z), "l"(a) : "memory");
+    a += 4;
+    asm("" : "+r"(a));
+    asm volatile("str %0, [%1]" ::"l"(z), "l"(a) : "memory");
+    a += 4;
+    asm("" : "+r"(a));
+    asm volatile("strb %0, [%1]" ::"l"(z), "l"(a) : "memory");
+    {
+      u8 fl = (p->s).flags;
+      u32 f = 0xfb;
+      f &= fl;
+      (p->s).flags = f;
+    }
+  }
+  switch ((p->s).mode[2]) {
+    case 0: {
+      register u8 d5 asm("r2");
+      d5 = *((u8*)p + 0xb5);
+      if (d5 == 1) {
+        u8 fl = (p->s).flags;
+        u32 f = 0xfe;
+        f &= fl;
+        (p->s).flags = f;
+        (p->s).work[3] = d5;
+        (p->s).mode[2] = 3;
+        break;
+      }
+      SetMotion(&p->s, 0xA903);
+      {
+        register s32 one asm("r2");
+        register s32 z asm("r4");
+        u8* yf;
+        u8* oa;
+        s32 ov;
+        s32 m21;
+        one = 1;
+        {
+          u8 fl = (p->s).flags;
+          u32 f = 0x20;
+          f |= fl;
+          (p->s).flags = f;
+        }
+        yf = (u8*)p + 0x4d;
+        z = 0;
+        *yf = one;
+        oa = (u8*)p + 0x4a;
+        one = 0x20;
+        ov = *oa;
+        m21 = -0x21;
+        m21 &= ov;
+        *oa = m21 | one;
+        UpdateMotionGraphic(&p->s);
+        (p->s).coord.x += -0x100;
+        (p->s).d.y = 0xe0 << 2;
+        (p->s).d.x = z;
+      }
+      goto inc;
+    }
+    case 1: {
+      s32 r;
+      (p->s).coord.y += (p->s).d.y;
+      UpdateMotionGraphic(&p->s);
+      r = PushoutToUp1((p->s).coord.x, (p->s).coord.y + (0x80 << 5));
+      if (r == 0) {
+        break;
+      }
+      (p->s).coord.y += r;
+      c1.x = (p->s).coord.x;
+      c1.y = (p->s).coord.y;
+      createHellbatElectricBeam(&p->s, &c1, 0, 1);
+      (p->s).work[3] = 0;
+      (p->s).work[2] = 1;
+      {
+        u8 fl = (p->s).flags;
+        u32 f = 0xfe;
+        f &= fl;
+        (p->s).flags = f;
+      }
+      goto inc;
+    }
+    case 2:
+      if ((p->s).work[2] != 0) {
+        s32 t = (p->s).work[2] - 1;
+        (p->s).work[2] = t;
+        if ((t << 24) != 0) {
+          break;
+        }
+      }
+      c2.x = (p->s).coord.x;
+      c2.y = (p->s).coord.y;
+      FUN_080a1538((p->s).unk_28, &c2, 1);
+      {
+        u8 fl = (p->s).flags;
+        u32 f = 1;
+        f |= fl;
+        (p->s).flags = f;
+      }
+      goto inc;
+    case 3: {
+      register s32 z asm("r4");
+      SetMotion(&p->s, 0xA905);
+      SetDDP(&p->body, (const struct Collision*)0x0836B2C4);
+      {
+        register s32 zz asm("r2");
+        u8* yf;
+        u8* oa;
+        s32 ov;
+        s32 m21;
+        zz = 0;
+        {
+          u8 fl = (p->s).flags;
+          u32 f = 0xdf;
+          f &= fl;
+          (p->s).flags = f;
+        }
+        yf = (u8*)p + 0x4d;
+        *yf = zz;
+        oa = (u8*)p + 0x4a;
+        ov = *oa;
+        m21 = -0x21;
+        m21 &= ov;
+        *oa = m21;
+      }
+      if ((p->s).work[3] == 1) {
+        (p->s).d.x = 0xc0 << 2;
+      } else {
+        (p->s).d.x = -0x300;
+      }
+      {
+        u8 fl = (p->s).flags;
+        u32 f = 1;
+        z = 0;
+        f |= fl;
+        (p->s).flags = f;
+      }
+      {
+        register s32 cx4 asm("r0");
+        register s32 cy4 asm("r1");
+        register s32 k4 asm("r2");
+        cx4 = (p->s).coord.x;
+        cy4 = (p->s).coord.y;
+        k4 = -0x400;
+        cy4 += k4;
+        (p->s).coord.y = FUN_08009f6c(cx4, cy4);
+      }
+      (p->s).unk_coord.x = z;
+      (p->s).work[2] = z;
+      (p->s).mode[2]++;
+      FALLTHROUGH;
+    }
+    case 4: {
+      s32 cx = (p->s).coord.x;
+      s32 dx = (p->s).d.x;
+      s32 acc;
+      s32 r;
+      (p->s).coord.x = cx + dx;
+      {
+        register s32 a0 asm("r0");
+        register s32 a1 asm("r1");
+        a0 = (p->s).unk_coord.x + dx;
+        (p->s).unk_coord.x = a0;
+        asm volatile("add %0, %1, #0" : "=&l"(a1) : "l"(a0));
+        if (a1 < 0) {
+          a1 = -a1;
+        }
+        acc = a1;
+      }
+      if (acc > (0xa0 << 5)) {
+        if ((p->s).work[2] == 0) {
+          struct Projectile* q;
+          (p->s).work[2] = 1;
+          q = FUN_080a1594(&p->s, &(p->s).coord, 0);
+          (q->s).d.x = (p->s).d.x;
+        }
+      }
+      UpdateMotionGraphic(&p->s);
+      if ((p->s).work[3] == 1) {
+        r = PushoutToLeft1((p->s).coord.x + (0x80 << 3), (p->s).coord.y);
+      } else {
+        r = PushoutToRight1((p->s).coord.x + -0x400, (p->s).coord.y);
+      }
+      if (r == 0) {
+        break;
+      }
+      (p->s).coord.x += r;
+      (p->s).d.y = -0x300;
+      goto inc;
+    }
+    case 5: {
+      register s32 s asm("r2");
+      s32 cy;
+      (p->s).coord.y += (p->s).d.y;
+      UpdateMotionGraphic(&p->s);
+      s = 0;
+      {
+        const struct Camera* cam = &gStageRun.vm.camera;
+        s32 lim = cam->viewport.y + -0x5000;
+        cy = (p->s).coord.y;
+        if (cy < lim) {
+          s = 1;
+        }
+      }
+      if (0) {
+        s = 1;
+      }
+      if (s == 0) {
+        break;
+      }
+      {
+        register s32 ny asm("r0");
+        ny = cy + s;
+        (p->s).coord.y = ny;
+      }
+      (p->s).unk_coord.x = 0xff;
+      {
+        register u8* a asm("r0");
+        register s32 z asm("r1");
+        a = (u8*)p + 0x8c;
+        z = 0;
+        asm volatile("str %0, [%1]" ::"l"(z), "l"(a) : "memory");
+        a += 4;
+        asm("" : "+r"(a));
+        asm volatile("str %0, [%1]" ::"l"(z), "l"(a) : "memory");
+        a += 4;
+        asm("" : "+r"(a));
+        asm volatile("strb %0, [%1]" ::"l"(z), "l"(a) : "memory");
+      }
+      {
+        u8 fl = (p->s).flags;
+        u32 f = 0xfb;
+        f &= fl;
+        (p->s).flags = f;
+      }
+    inc:
+      (p->s).mode[2]++;
+      break;
+    }
+    case 6: {
+      s32 v;
+      UpdateMotionGraphic(&p->s);
+      {
+        register u16* m asm("r1");
+        register s32 v0 asm("r0");
+        v0 = *(volatile s32*)&(p->s).unk_coord.x;
+        m = (u16*)((u8*)p + 0x50);
+        *m = v0;
+        v0 = *(volatile s32*)&(p->s).unk_coord.x;
+        m = (u16*)((u8*)m + 2);
+        asm("" : "+r"(m));
+        *m = v0;
+      }
+      v = *(volatile s32*)&(p->s).unk_coord.x;
+      if (v > 0) {
+        (p->s).unk_coord.x = v - 0x20;
+      }
+      if ((p->s).unk_coord.x > 0x20) {
+        break;
+      }
+      SET_PROJECTILE_ROUTINE(p, ENTITY_DIE);
+      break;
+    }
+  }
+}
 
 void FUN_080a22ec(struct Projectile* p) {
   (p->s).mode[1] = 1;
